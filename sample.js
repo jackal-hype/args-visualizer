5c0ab904a3824c4db0df272d1aeae01cf2713ac3
kc <kcpi9001@yandex.ru>
2019-05-04 21:42:39 +0300

    first

---
+++ sample.js
@@ -0,0 +1,4 @@
+console.log("NodeJS installed");
+let a = 2, b = 3;
+let s = a + b;
+console.log("sum: " + s);
---
+++ server.js
@@ -0,0 +1,28 @@
+if (process.env.NODE_ENV !== 'production') {
+    const dotenv = require('dotenv')
+    // console.log("dotenv: ", dotenv)
+    dotenv.config()
+}
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use('/', indexRouter)
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true
+})
+const db = mongoose.connection
+db.on('error', error => console.error(error) )
+db.once('open', () => console.log('connected to Mongoose') )
+
+app.listen(process.env.PORT || 3000)
+
